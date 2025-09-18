e7677d67ad3a2373a11fd217ca62d87ee54018fb
anjul79 <anjul79@gmail.com>
2025-09-18 14:54:07 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -422,4 +422,21 @@ const Contact = () => {
   );
 };
 
+
+    <iframe
+      id="JotFormIFrame-252601563591456"
+      title="Modern Contact Form"
+      onload="window.parent.scrollTo(0,0)"
+      allowtransparency="true"
+      allow="geolocation; microphone; camera; fullscreen; payment"
+      src="https://form.jotform.com/252601563591456"
+      frameborder="0"
+      style="min-width:100%;max-width:100%;height:539px;border:none;"
+      scrolling="no"
+    >
+    </iframe>
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
+    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-252601563591456']", "https://form.jotform.com/")</script>
+    
+
 export default Contact;
